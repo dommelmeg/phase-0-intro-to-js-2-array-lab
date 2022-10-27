204024efdbf04bbb51a60695d258e3dc0cb37453
Megan <dommelmeg@gmail.com>
2022-10-26 21:55:34 -0400

    commit tree f1b15224b4610859abbdc91c41a006ea8b601519

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => cats.push(name);
+
+const destructivelyPrependCat = name => cats.unshift(name);
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = name => [...cats, name];
+
+const prependCat = name => [name, ...cats];
+
+function removeLastCat() {
+    const newCat = cats.slice(0, 2);
+
+    return newCat;
+}
+
+function removeFirstCat() {
+    const newerCat = cats.slice(1);
+
+    return newerCat;
+}
